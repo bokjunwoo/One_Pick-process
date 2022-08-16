801f2dfb4792356c6097028aedb44be27e02be32
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-08-17 01:38:58 +0900

    추가

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const playMovie  = document.querySelector(".mr_movie");
+
+playMovie.addEventListener('click', function(){
+    playMovie.requestFullscreen();
+    playMovie.play();
+})
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+//scroll trigger
+const boxScroll = gsap.timeline();
+
+boxScroll.to(".box1", {
+    duration: 1,
+    scale: 3,
+    x: 300,
+    y: 500,
+}).to(".box2", {
+    duration: 1,
+    scale: 3,
+    x: 500,
+    y: 200,
+});
+
+ScrollTrigger.create({
+    animation: boxScroll,
+    trigger: ".section_trigger",
+    start: "top top ",
+    end: "+=1000",
+    scrub: 2,
+    markers: true,
+    pin: true,
+});
+    
+    
+
